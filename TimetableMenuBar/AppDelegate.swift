02c11ac95eb2597a2102eabc4394fa714d069def
mebsim <mebsim@users.noreply.github.com>
2017-10-23 09:20:22 -0400

    Initial Commit

---
+++ TimetableMenuBar/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TimetableMenuBar
+//
+//  Created by Mohamed Ebsim on 2017-10-23.
+//  Copyright © 2017 Mohamed Ebsim. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ TimetableMenuBar/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TimetableMenuBar
+//
+//  Created by Mohamed Ebsim on 2017-10-23.
+//  Copyright © 2017 Mohamed Ebsim. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
